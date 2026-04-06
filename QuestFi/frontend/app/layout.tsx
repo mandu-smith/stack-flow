1faecd28d5ce43942e32fae49b54b22896cd0063
mandu-smith <mandusmithsambo@gmail.com>
2026-04-06 08:34:48 +0100

    feat: add layout component with initial imports for Next.js application

---
+++ QuestFi/frontend/app/layout.tsx
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
+import ConditionalFooter from "@/components/layout/ConditionalFooter";
+import { TurnkeyProvider } from "@/providers/TurnkeyProvider";
+import { OAuthCallbackHandler } from "@/components/auth/OAuthCallbackHandler";
