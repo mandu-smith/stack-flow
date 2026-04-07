169466a7757a590743289b8705d35053cda9a47c
mandu-smith <mandusmithsambo@gmail.com>
2026-04-07 21:19:50 +0100

    Remove unused configuration files and TurnkeyProvider component

--- QuestFi/frontend/app/quests/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProtocolCards from '@/components/landing/ProtocolCards'
-
-export default function QuestsPage() {
-  return (
-    <div className="min-h-screen pt-16 bg-black">
-      <ProtocolCards />
-    </div>
-  )
-}
