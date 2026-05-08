47f6f09fe62f7fb165b1373fc86c69e01d16329f
mandu-smith <mandusmithsambo@gmail.com>
2026-05-08 17:18:19 +0100

    refactor: fix duplicate case handling in Dismiss action of reducer in useToast hook

--- frontend/src/hooks/use-toast.ts
+++ frontend/src/hooks/use-toast.ts
@@ -72,3 +72,16 @@ export const reducer = (state: State, action: Action): State => {
 
       case "DISMISS_TOAST": {
       const { toastId } = action;
+
+      case "DISMISS_TOAST": {
+      const { toastId } = action;
+
+      // ! Side effects ! - This could be extracted into a dismissToast() action,
+      // but I'll keep it here for simplicity
+      if (toastId) {
+        addToRemoveQueue(toastId);
+      } else {
+        state.toasts.forEach((toast) => {
+          addToRemoveQueue(toast.id);
+        });
+      }
