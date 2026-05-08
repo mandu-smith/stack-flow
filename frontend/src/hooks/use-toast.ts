6a071342e2acd17415b53a4065f8d1575ebf9db4
mandu-smith <mandusmithsambo@gmail.com>
2026-05-08 17:20:37 +0100

    refactor: add useEffect to manage listener subscription in useToast hook

--- frontend/src/hooks/use-toast.ts
+++ frontend/src/hooks/use-toast.ts
@@ -147,3 +147,13 @@ function toast({ ...props }: Toast) {
 
   function useToast() {
   const [state, setState] = React.useState<State>(memoryState);
+
+   React.useEffect(() => {
+    listeners.push(setState);
+    return () => {
+      const index = listeners.indexOf(setState);
+      if (index > -1) {
+        listeners.splice(index, 1);
+      }
+    };
+  }, [state]);
